059ee8c20f851f10672dc355a65325fbf970fe1d
Lenard Luke Llanillo <luke@neoalphaglobal.com>
2024-07-12 20:07:28 +0800

    chore: Add jpg as a image extension

--- specsheet/utils/image_utils.py
+++ specsheet/utils/image_utils.py
@@ -7,7 +7,7 @@ from PIL import Image, UnidentifiedImageError
 def get_image_base64(image):
     try:
         image_io = io.BytesIO(image)
-        image_io.seek(0)  # Ensure the pointer is at the start
+        image_io.seek(0)
         image = Image.open(image_io)
 
         buffered = io.BytesIO()
@@ -15,15 +15,17 @@ def get_image_base64(image):
             image.save(buffered, format="PNG")
         elif image.format == "JPEG":
             image.save(buffered, format="JPEG")
+        elif image.format == "JPG":
+            image.save(buffered, format="JPG")
         else:
             raise UnidentifiedImageError("Unsupported image format")
 
         return base64.b64encode(buffered.getvalue()).decode()
     except UnidentifiedImageError as e:
         print(f"Error: Cannot identify image file. {e}")
-        # Optionally, return a placeholder image or an error message
+
         return None
     except Exception as e:
         print(f"Unexpected error: {e}")
-        # Optionally, return a placeholder image or an error message
+
         return None
